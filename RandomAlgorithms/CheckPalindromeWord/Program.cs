203da60ad3b1f53fb888d6caba8c1e9983fe062f
OKWUKWE ANYASI <okwukweanyasi@users.noreply.github.com>
2020-09-02 18:44:12 +0100

    added Palidrome checker program

---
+++ RandomAlgorithms/CheckPalindromeWord/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace CheckPalindromeWord
+{
+    class Program
+    {
+        //this code checks a given sets of words and returns if or not they are palindroms
+        static void Main(string[] args)
+        {
+            string[] words = { "civic", "ivicc", "civil", "livci", "tmp", "ovo", "ooo", "aaaa", "aaaa8" };
+
+
+
+            foreach (var word in words)
+            {
+
+                Console.WriteLine(word + " is palindrome?  " + isPalindrome(word) + "\n");
+            }
+        }
+
+        static bool isPalindrome(string inputString)
+        {
+
+            int oddCount = 0;
+
+
+
+            var keyValues = inputString.ToLower().ToCharArray().GroupBy(k => k)
+
+            .ToDictionary(g => g.Key, g => g.Count());
+
+
+
+            foreach (var keyValue in keyValues)
+
+            {
+
+                if ((keyValue.Value % 2) != 0) //count is odd
+
+                {
+
+                    oddCount++;
+
+                    if (oddCount > 1) // palindrome rule broken. Word cant be a palindrome
+
+                        return false;
+
+                }
+
+            }
+
+            return true;
+
+        }
+    }
+
+
+}
---
+++ RandomAlgorithms/RandomAlgorithms/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RandomAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //string[] words = new string[] { "W", " o", "r", "d" };
+            char words ="Word";
+
+            //            def rec(value):
+            //    if value == 0:
+            //        return 1
+            //    return rec(value - 1) * value
+            //print(rec(3))
+            int permutationsSize = 1;
+            for(int i = 0; i < words.Length; i++){
+                permutationsSize = permutationsSize * (i+1);
+            }
+            int[] permutatedWords = new int[permutationsSize];
+
+            int currentIterationSize = words.Length;
+            for(int i = 0; i < words.Length; i++){
+
+            }
+
+        }
+
+
+
+        string[] BaseFActorial(string first, string second)
+        {
+            string[] result = new string[2];
+            result[0] = $"{ first}{ second}";
+            result[1] = $"{ second}{ first}";
+
+            return result;
+        }
+
+    }
+}
