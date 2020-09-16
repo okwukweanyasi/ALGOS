e065324e9c5761f611eebb60629680151a0f8e7e
OKWUKWE ANYASI <okwukweanyasi@users.noreply.github.com>
2020-09-16 13:48:23 +0100

    implemented Enqueue Dequeue method

--- RandomAlgorithms/QueueAndStacks/Program.cs
+++ RandomAlgorithms/QueueAndStacks/Program.cs
@@ -7,31 +7,33 @@ namespace QueueAndStacks
     {
         public static int stack1Count = 0;
         public static Stack stack1 = new Stack();
-         public static Stack stack2 = new Stack();
+        public static Stack stack2 = new Stack();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string[] items = {"1","2","3","4","5"};
+            string[] items = { "1", "2", "3", "4", "5" };
 
 
-            foreach (var i in items)
-            {
+            //foreach (var i in items)
+            //{
 
-                stack1.Push(i);
-                stack1Count++;
+            //    stack1.Push(i);
+            //    stack1Count++;
 
-            }
+            //}
+            EnqueStack(items);
 
             // Displays the Queue.
             Console.Write("Stack 1 values:");
             PrintValues(stack1);
 
-            for (int i = 0; i < stack1Count; i++)
-            {
+            DequeStack(stack1);
+            //for (int i = 0; i < stack1Count; i++)
+            //{
 
-                stack2.Push(stack1.Pop());
+            //    stack2.Push(stack1.Pop());
 
-            }
+            //}
 
             // Displays the Queue.
             Console.Write("Stack 2 values:");
@@ -40,7 +42,30 @@ namespace QueueAndStacks
 
         }
 
+        public static Stack EnqueStack(string[] inputStream)
+        {
+            //Stack stack = new Stack();
+            foreach (var i in inputStream)
+            {
 
+                stack1.Push(i);
+                stack1Count++;
+
+            }
+
+            return stack1;
+        }
+
+        public static Stack DequeStack(Stack stack)
+        {
+            for (int i = 0; i < stack1Count; i++)
+            {
+
+                stack2.Push(stack1.Pop());
+
+            }
+            return stack2;
+        }
 
         public static void PrintValues(IEnumerable myCollection)
         {
