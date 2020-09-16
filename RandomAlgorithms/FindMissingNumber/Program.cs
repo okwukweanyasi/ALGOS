75e9311f30c4197605778c153f16d52ba4198201
OKWUKWE ANYASI <okwukweanyasi@users.noreply.github.com>
2020-09-16 13:42:02 +0100

    added "find missing nos", "Implementing queue with stack"

---
+++ RandomAlgorithms/FindMissingNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FindMissingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //string val;
+            //Console.Write("Enter integer: ");
+            //val = Console.ReadLine();
+            int[] numbers=   new int[] { 3, 7, 1, 2, 8, 4, 5 };
+
+           int missingNo=  MissingNumber(numbers);
+
+            Console.WriteLine($"The missing number is:{missingNo}");
+            Console.WriteLine("Press any key to exit");
+            string a = Console.ReadLine();
+
+        }
+
+        static int MissingNumber(int[] nosArrray)
+        {
+            int missingNo=0;
+
+            Array.Sort(nosArrray);
+
+            for (int i=0; i< nosArrray.Length-1; i++)
+            {
+                if ((nosArrray[i+1] - nosArrray[i]) > 1)
+                {
+                    missingNo = nosArrray[i] +1;
+                }
+            }
+
+            return missingNo;
+            ;
+        }
+    }
+}
---
+++ RandomAlgorithms/QueueAndStacks/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueAndStacks
+{
+    class Program
+    {
+        public static int stack1Count = 0;
+        public static Stack stack1 = new Stack();
+         public static Stack stack2 = new Stack();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] items = {"1","2","3","4","5"};
+
+
+            foreach (var i in items)
+            {
+
+                stack1.Push(i);
+                stack1Count++;
+
+            }
+
+            // Displays the Queue.
+            Console.Write("Stack 1 values:");
+            PrintValues(stack1);
+
+            for (int i = 0; i < stack1Count; i++)
+            {
+
+                stack2.Push(stack1.Pop());
+
+            }
+
+            // Displays the Queue.
+            Console.Write("Stack 2 values:");
+            PrintValues(stack2);
+
+
+        }
+
+
+
+        public static void PrintValues(IEnumerable myCollection)
+        {
+            foreach (Object obj in myCollection)
+                Console.Write("    {0}", obj);
+            Console.WriteLine();
+        }
+    }
+}
