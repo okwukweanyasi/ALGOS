49402fb60764c565b44ba7f2028cda9efcaf77c5
OKWUKWE ANYASI <okwukweanyasi@users.noreply.github.com>
2020-09-27 23:50:46 +0100

    Implement BST to find missing number

---
+++ RandomAlgorithms/FindDuplicateNoBST/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FindDuplicateNoBST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 3, 7, 1, 3, 2, 8, 4, 5, 6 };
+            int[] numbers1 = new int[] { 3, 7, 1, 8, 2, 4, 5, 6, 8 };
+            int[] numbers2 = new int[] { 3, 10, 7, 4, 1, 2, 4, 9, 5, 6, 8 };
+        }
+    }
+}
