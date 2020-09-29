be62ec538d163bbc2f7f8796248e2115ecff6731
OKWUKWE ANYASI <okwukweanyasi@users.noreply.github.com>
2020-09-29 23:54:00 +0100

    add

---
+++ RandomAlgorithms/AutoComplete/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoComplete
+{
+    class Program
+    {
+        static Dictionary<string, Dictionary<string, string>> matchedWordsDictPair = new Dictionary<string, Dictionary<string, string>>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<string> wordsRepository = new List<string> { "dog", "deer", "deal", "deer","doggy", "fiasco", "fiachetto" };
+            string searchword = "de";
+            Dictionary<string, string> result = FindWords(searchword, wordsRepository);
+            Console.WriteLine($"result: {result}");
+            foreach (var res in result)
+            {
+                Console.WriteLine("Search Word {0}: returned Words = {1}", res.Key, res.Value);
+            }
+
+            var result1 = FindWordsOptimized(searchword, wordsRepository);
+            foreach (var res in result1)
+            {                
+                Console.WriteLine("Search Word {0}: returned Words = {", res.Key);
+                foreach (var r in res.Value)
+                {
+                    Console.Write("[Key {0}: Value{1}]", r.Key, r.Value);
+                }
+            }
+            Console.WriteLine("Press any key to exit");
+            string a = Console.ReadLine();
+        }
+
+        public static Dictionary<string, Dictionary<string,string>> FindWordsOptimized(string inputString, List<string> repo)
+        {
+           
+            int inputLenght = inputString.Length;
+
+            foreach (string r in repo)
+            {
+                string prefixWord = r.Substring(0, inputLenght);
+                if (string.Equals(prefixWord, inputString, StringComparison.OrdinalIgnoreCase))
+                {
+                    if (matchedWordsDictPair.ContainsKey(inputString))
+                    {
+                        Dictionary<string, string> matchwordArray = matchedWordsDictPair[inputString];
+                        if (!matchwordArray.ContainsKey(r))
+                        {
+                            matchwordArray.Add(r, r);
+                        }
+                    }
+                    else
+                    {
+                        Dictionary<string, string> matchwordArray = new Dictionary<string, string>();
+                        matchwordArray.Add(r, r);
+                        matchedWordsDictPair.Add(inputString, matchwordArray);
+                    }
+                }
+            }
+
+            return matchedWordsDictPair;
+        }
+        public static Dictionary<string,string> FindWords(string inputString, List<string> repo)
+        {
+            Dictionary<string, string> matchedWords = new Dictionary<string, string>();
+            int inputLenght = inputString.Length;
+            
+            foreach (string r in repo)
+            {
+                string prefixWord = r.Substring(0, inputLenght);
+                if (string.Equals(prefixWord, inputString, StringComparison.OrdinalIgnoreCase))
+                {
+                    matchedWords.Add(r, r);
+                }
+            }
+
+            return matchedWords;
+        }
+    }
+}
--- RandomAlgorithms/BST/Program.cs
+++ RandomAlgorithms/BST/Program.cs
@@ -7,6 +7,49 @@ namespace BST
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            BinaryTree binaryTree = new BinaryTree();
+
+            binaryTree.Insert(75);
+            binaryTree.Insert(57);
+            binaryTree.Insert(90);
+            binaryTree.Insert(32);
+            binaryTree.Insert(7);
+            binaryTree.Insert(44);
+            binaryTree.Insert(60);
+            binaryTree.Insert(86);
+            binaryTree.Insert(93);
+            binaryTree.Insert(99);
+            binaryTree.Insert(100);
+        }
+
+        public static int getHeight(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(getHeight(root.LeftNode), getHeight(root.RightNode)) + 1;
+        }
+
+        public static bool isBalanced(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            int heightDiff = getHeight(root.LeftNode) - getHeight(root.RightNode);
+            if (Math.Abs(heightDiff) > 1)
+            {
+                return false;
+            }
+            else
+            {
+                return (isBalanced(root.LeftNode) && isBalanced(root.RightNode));
+            }
         }
     }
 }
