b3cedf5d5acbb8ad9435e97c00682c5c53fd13e6
OKWUKWE ANYASI <okwukweanyasi@users.noreply.github.com>
2022-01-30 12:33:26 +0100

    update

--- RandomAlgorithms/FindMissingNo/Program.cs
+++ RandomAlgorithms/FindMissingNo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FindMissingNo
 {
@@ -11,6 +13,25 @@ namespace FindMissingNo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            List<int> L1 = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            List<int> L2 = new List<int> { 1, 4, 5, 6, 2, 7, 8, 9 };
+
+            Console.WriteLine(MissingNumber(L1, L2));
+        }
+
+        public static int MissingNumber(List<int> l1, List<int> l2)
+        {
+            int missingValue = 0;
+            int l1Sum = l1.Sum();
+            int l2Sum = l2.Sum();
+
+            //Console.WriteLine("l1Sum: " +l1Sum);
+            //Console.WriteLine("l2Sum: " +l2Sum);
+
+            missingValue = l1Sum - l2Sum;
+
+            return missingValue;
         }
     }
 }
