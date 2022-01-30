02f6e66ec6e21095f48b296efd807317239a0e7a
OKWUKWE ANYASI <okwukweanyasi@users.noreply.github.com>
2022-01-30 12:31:34 +0100

    new prj

---
+++ RandomAlgorithms/FindMissingNo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FindMissingNo
+{
+    class Program
+    {
+        // inputs 2 lists. L1, complete set of integers, L2, Duplicate of L2, missing 1. Return the missing Item
+        //L1: 1,2,3,4,5,6//21
+
+        //L2:3,4,5,6,2 //20
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
